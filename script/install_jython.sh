7597145478c0964e1191bca1e7030307ba84dcbf
ZZy979 <979481894@qq.com>
2024-05-11 00:15:57 +0800

    添加install_jython.sh

---
+++ script/install_jython.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+url="https://repo1.maven.org/maven2/org/python/jython-installer/2.7.3/jython-installer-2.7.3.jar"
+jar_file=jython-installer-2.7.3.jar
+install_dir=$HOME/jython
+
+cd /tmp
+wget $url
+java -jar $jar_file -s -d $install_dir
+rm $jar_file
